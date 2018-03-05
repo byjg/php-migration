5cdcdebb2634c0f1b45d4c0c169e99928e10019a
Joao Gilberto Magalhães <joao@byjg.com.br>
2018-03-05 14:07:38 -0300

    Added automated tests for MySQL, PostgresSQL and Microsoft SQL Server

--- tests/SqlServerDatabaseTest.php
+++ tests/SqlServerDatabaseTest.php
@@ -11,14 +11,6 @@ class SqlServerDatabaseTest extends BaseDatabase
      */
     protected $migrate = null;
 
-    public function getExpectedUsersVersion1()
-    {
-        return [
-            ["id" => 1, "name" => 'John Doe', 'createdate' => 'Jan 10 2016 12:00:00:AM'],
-            ["id" => 2, "name" => 'Jane Doe', 'createdate' => 'Dec 30 2015 12:00:00:AM']
-        ];
-    }
-
     public function setUp()
     {
         $this->migrate = new \ByJG\DbMigration\Migration(new \ByJG\Util\Uri($this->uri), __DIR__ . '/../example/sql_server');
