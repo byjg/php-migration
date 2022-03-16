0e90d54eb1e6c198c6731035c29a0d0a2887e9dc
Joao Gilberto Magalhaes <joao@byjg.com.br>
2022-03-16 18:36:53 -0500

    Fix tests with PHPUnit 9 (+3)

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -10,19 +10,22 @@ fi
 
 n=0;
 max=10
+secs=6
 while [ -z "$(docker ps -q -f health=healthy -f name=anydataset_db_$1)" ] && [ "$n" -lt "$max" ];
 do
   echo "Waiting for $1...";
   n=$(( n + 1 ))
-  sleep 5;
+  sleep $secs;
 done
 
 if [ "$n" -gt "$max" ]
 then
-  echo "$mysql was not health after $(( max * 5 ))"
+  echo "$mysql was not health after $(( max * secs ))"
   exit 2
 fi
 
 echo "$1 is up"
+
+docker ps
 
 
